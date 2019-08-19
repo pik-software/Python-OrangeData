7d78514baf543e4ae0e10adeeb4afb92d13729b8
goalkeeer <goalkeeer@gmail.com>
2019-08-19 16:40:31 +0500

    version="2.1.2"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="OrangeData",
-    version="2.1.1",
+    version="2.1.2",
     url="https://github.com/vanzhiganov/Python-OrangeData",
     packages=[
         'OrangeData'
